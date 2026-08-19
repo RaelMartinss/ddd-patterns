a17d03426af890651b41541f3d928612618c6ce9
RaelMartinss <raelpgm2@gmail.com>
2026-08-18 21:11:08 -0300

    test(order-item) associate item with product

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -15,10 +15,6 @@ export default class Order {
         this.validate();
     }
 
-    // get id(): string { return this._id; }
-    // get customerId(): string { return this._customerId; }
-    // get items(): OrderItem[] { return this._items; }
-
     validate(): boolean {
         if (this._id.length === 0) {
             throw new Error("Id is required");
@@ -32,11 +28,15 @@ export default class Order {
             throw new Error("Items are required");
         }
 
+        if (this._items.some((item) => item.quantity <= 0)) {
+            throw new Error("Quantity must be greater than 0");
+        }
+
         return true;
     }
 
     total(): number {
-        return this._items.reduce((acc, item) => acc + item.price, 0);
+        return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
     }
 
 }
