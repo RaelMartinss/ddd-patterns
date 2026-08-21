53414f433e19ab0b69914dd7eda95a519d947e70
RaelMartinss <raelpgm2@gmail.com>
2026-08-21 18:41:55 -0300

    feat(product): expose product identifier
    Adiciona o getter do identificador para permitir o mapeamento da entidade no repositório.

--- src/domain/entity/product.ts
+++ src/domain/entity/product.ts
@@ -10,7 +10,7 @@ export default class Product {
         this._price = price;
         this.validate();
     }   
-
+    get id(): string { return this._id}
     get name(): string { return this._name; }
     get price(): number { return this._price; }
 
